a7703b8d03444aa3b2e49455b58d6b1178343300
Darin McGill <darin.mcgill@gmail.com>
2014-01-08 15:26:26 -0600

    return value stuff

---
+++ sqlTest.sql
@@ -0,0 +1,27 @@
+create or replace function
+exsp_test_one(x int,y int) 
+RETURNS int as $$
+BEGIN
+    return x*x+y;
+END;
+$$ LANGUAGE plpgsql;
+
+select exsp_test_one(y:=10,x:=6);
+
+drop table if exists tbl_test cascade;
+create table if not exists tbl_test (
+    foo varchar(100),
+    bar int
+);
+insert into tbl_test values ('cheese',7),('fries',13);
+
+create or replace function
+exsp_test_many()
+RETURNS SETOF tbl_test
+as $body$
+BEGIN
+    return query select * from tbl_test limit 1;
+END;
+$body$ LANGUAGE plpgsql;
+
+select exsp_test_many();
